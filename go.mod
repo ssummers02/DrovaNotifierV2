acc0b64027e22c6663a6dd912d906d8d12d5c51d
IceBearG <65737922+IceBeerG@users.noreply.github.com>
2023-11-09 17:03:59 +0400

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Drova_Notifier
+
+go 1.21.2
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
---
+++ main.go
@@ -0,0 +1,385 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+	"unsafe"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+var (
+	BotToken, fileConfig, fileGames string
+	serverID, authToken, hostname   string
+	Chat_IDint                      int64
+	kernel32                        = syscall.NewLazyDLL("kernel32.dll")
+	procSetConsoleTitleW            = kernel32.NewProc("SetConsoleTitleW")
+	isRunning                       bool
+)
+
+const (
+	appName    = "ese.exe"                                            // Имя запускаемого файла
+	newTitle   = "Drova Notifier v2"                                  // Имя окна программы
+	timeFormat = "02.01.2006 15:04:05"                                // Формат даты
+	url        = "https://services.drova.io/session-manager/sessions" // инфо по сессиям
+)
+
+type Product struct {
+	ProductID string `json:"productId"`
+	Title     string `json:"title"`
+}
+
+type IPInfoResponse struct {
+	IP     string `json:"ip"`
+	City   string `json:"city"`
+	Region string `json:"region"`
+	ISP    string `json:"org"`
+}
+
+// структура для выгрузки информации по сессиям
+type SessionsData struct {
+	Sessions []struct {
+		Client_id     string `json:"client_id"`
+		Product_id    string `json:"product_id"`
+		Created_on    int64  `json:"created_on"`
+		Finished_on   int64  `json:"finished_on"` //or null
+		Status        string `json:"status"`
+		Creator_ip    string `json:"creator_ip"`
+		Abort_comment string `json:"abort_comment"` //or null
+		Billing_type  string `json:"billing_type"`  // or null
+	}
+}
+
+func main() {
+	// Следующие 3 строки вводим свои данные. Чат ID будет с - в начале если это общий чат, и без - если это личка
+	authToken = "xxxxxxxx-xxxxx-xxxx-xxxx-xxxxxxxxxx"      // токен для авторизации на сайте
+	BotToken = "xxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" // токен бота
+	Chat_IDint = -00000000000000                           // ID чата получателя
+
+	setConsoleTitle(newTitle) // Устанавливаем новое имя окна
+
+	logFilePath := "errors.log" // Имя файла для логирования ошибок
+	logFilePath = filepath.Join(filepath.Dir(os.Args[0]), logFilePath)
+
+	// Открываем файл для записи логов
+	logFile, err := os.OpenFile(logFilePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal("Ошибка открытия файла", err, getLine())
+	}
+	defer logFile.Close()
+	// Получаем текущую директорию программы
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal("Ошибка получения текущей деректории: ", err, getLine())
+	}
+
+	// Устанавливаем файл в качестве вывода для логгера
+	log.SetOutput(logFile)
+
+	log.Println("Start program")
+	fmt.Println("Start program")
+	fileGames = filepath.Join(dir, "games.txt")
+	fileConfig = filepath.Join(dir, "config.txt")
+	fmt.Println(fileGames)
+	log.Println(fileGames)
+	fmt.Println(fileConfig)
+	log.Println(fileConfig)
+	fmt.Println()
+	gameID(fileGames) // получение списка ID игры - Название игры и сохранение в файл gamesID.txt
+
+	//блок для получения данных из конфига
+	/*
+		authToken, _ = readConfig("tokensite", config) // получаем токен для авторизации на сайте
+		BotToken, _ = readConfig("tokenbot", config)   // получаем токен этого бота
+		Chat_ID, _ = readConfig("chatID", config)      // определяем ID получателя
+		Chat_IDint, err := strconv.ParseInt(Chat_ID, 10, 64) // конвертируем ID чата в int64
+		if err != nil {
+			log.Println("Error: ", err, getLine())
+		}
+	*/
+
+	// Получаем имя ПК
+	hostname, err = os.Hostname()
+	if err != nil {
+		log.Println("Ошибка при получении имени компьютера: ", err, getLine())
+		return
+	}
+	serverID, _ = readConfig(hostname, fileConfig) // смотря на имя ПК ищем ID станции
+
+	for {
+		for i := 0; i != 2; { //ждем запуска приложения ese.exe
+			time.Sleep(5 * time.Second)                // интервал проверки запущенного процесса
+			isRunning = checkIfProcessRunning(appName) // запущено ли приложение
+			if isRunning {
+
+				chatMessage := sessionInfo("Start")
+				err := SendMessage(BotToken, Chat_IDint, chatMessage)
+				if err != nil {
+					log.Fatal("Ошибка отправки сообщения: ", err, getLine())
+				}
+				i = 2 //т.к. приложение запущено, выходим из цикла
+			}
+		}
+
+		// ждем закрытия процесса ese.exe
+		for i := 0; i != 3; {
+			isRunning = checkIfProcessRunning(appName)
+			if !isRunning {
+				time.Sleep(60 * time.Second) // задержка перед получением данных, на случай написания отзыва
+				chatMessage := sessionInfo("Stop")
+				err := SendMessage(BotToken, Chat_IDint, chatMessage)
+				if err != nil {
+					log.Fatal("Ошибка отправки сообщения: ", err, getLine())
+				}
+				i = 3
+			}
+
+			time.Sleep(5 * time.Second) // интервал проверки запущенного процесса
+		}
+	}
+}
+
+// Проверяет, запущен ли указанный процесс
+func checkIfProcessRunning(processName string) bool {
+	cmd := exec.Command("tasklist")
+	output, err := cmd.Output()
+	if err != nil {
+		log.Fatal("Ошибка получения списка процессов:", err, getLine())
+	}
+
+	return strings.Contains(string(output), processName)
+}
+
+// получаем данные из файла в виде ключ = значение
+func readConfig(keys, filename string) (string, error) {
+	var gname string
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Println("Ошибка при открытии файла ", filename, ": ", err, getLine())
+		fmt.Println("Ошибка при открытии файла ", filename, ": ", err)
+		return "Ошибка при открытии файла: ", err
+	}
+	defer file.Close()
+
+	// Создать сканер для чтения содержимого файла построчно
+	scanner := bufio.NewScanner(file)
+
+	// Создать словарь для хранения пары "ключ-значение"
+	data := make(map[string]string)
+
+	// Перебирать строки из файла
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, " = ")
+		if len(parts) == 2 {
+			key := parts[0]
+			value := parts[1]
+			data[key] = value
+		}
+	}
+
+	if value, ok := data[keys]; ok {
+		gname = value
+	}
+	return gname, err
+}
+
+func SendMessage(botToken string, chatID int64, text string) error {
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Println("Ошибка подключения бота: ", err, getLine())
+		return err
+	}
+
+	message := tgbotapi.NewMessage(chatID, text)
+
+	_, err = bot.Send(message)
+	if err != nil {
+		log.Println("Ошибка отправки сообщения: ", err, getLine())
+		return err
+	}
+
+	return nil
+}
+
+// для смены заголовока программы
+func setConsoleTitle(title string) {
+	ptrTitle, _ := syscall.UTF16PtrFromString(title)
+	_, _, _ = procSetConsoleTitleW.Call(uintptr(unsafe.Pointer(ptrTitle)))
+}
+
+// получение строки кода где возникла ошибка
+func getLine() int {
+	_, _, line, _ := runtime.Caller(1)
+	return line
+}
+
+func gameID(fileGames string) {
+	// Отправить GET-запрос на API
+	resp, err := http.Get("https://services.drova.io/product-manager/product/listfull2")
+	if err != nil {
+		fmt.Println("Ошибка при выполнении запроса:", err, getLine())
+		return
+	}
+	defer resp.Body.Close()
+
+	// Прочитать JSON-ответ
+	var products []Product
+	err = json.NewDecoder(resp.Body).Decode(&products)
+	if err != nil {
+		fmt.Println("Ошибка при разборе JSON-ответа:", err, getLine())
+		return
+	}
+	// Создать файл для записи
+	file, err := os.Create(fileGames)
+	if err != nil {
+		fmt.Println("Ошибка при создании файла:", err, getLine())
+		return
+	}
+	defer file.Close()
+
+	// Записывать данные в файл
+	for _, product := range products {
+		line := fmt.Sprintf("%s = %s\n", product.ProductID, product.Title)
+		_, err = io.WriteString(file, line)
+		if err != nil {
+			fmt.Println("Ошибка при записи данных в файл:", err, getLine())
+			return
+		}
+	}
+	time.Sleep(1 * time.Second)
+}
+
+func sessionInfo(status string) (infoString string) {
+	// Создание HTTP клиента
+	client := &http.Client{}
+
+	// Создание нового GET-запроса
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("Failed to create request: ", err, getLine())
+	}
+
+	// Установка параметров запроса
+	q := req.URL.Query()
+	q.Add("server_id", serverID)
+	req.URL.RawQuery = q.Encode()
+
+	// Установка заголовка X-Auth-Token
+	req.Header.Set("X-Auth-Token", authToken)
+
+	// Отправка запроса и получение ответа
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Failed to send request: ", err, getLine())
+	}
+	defer resp.Body.Close()
+
+	// Запись ответа в строку
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, resp.Body)
+	if err != nil {
+		log.Fatal("Failed to write response to buffer: ", err, getLine())
+	}
+
+	responseString := buf.String()
+	var data SessionsData                         // структура SessionsData
+	json.Unmarshal([]byte(responseString), &data) // декодируем JSON файл
+	// var sessionOff, sessionDur string = "", ""
+
+	comment := strings.ReplaceAll(data.Sessions[0].Abort_comment, ";", ":")
+	sessionOn, _ := dateTimeS(data.Sessions[0].Created_on)
+	game, _ := readConfig(data.Sessions[0].Product_id, fileGames)
+	city, asn, region := ipInf(data.Sessions[0].Creator_ip)
+
+	// формируем текст для отправки
+	if status == "Start" {
+		infoString = hostname + " - " + data.Sessions[0].Creator_ip + "\n\n" + sessionOn + "\nИгра: " + game
+		infoString += "\nГород: " + city + "\nРегион: " + region + "\nПровайдер: " + asn + "\nОплата: " + data.Sessions[0].Billing_type
+		fmt.Println(infoString)
+		fmt.Println()
+		return
+	} else { // иначе, записываем значение окончания сессии и высчитываем продолжительность сессии
+		// sessionOff, _ := dateTimeS(data.Sessions[0].Finished_on)
+		_, stopTime := dateTimeS(data.Sessions[0].Finished_on)
+		_, startTime := dateTimeS(data.Sessions[0].Created_on)
+		sessionDur := dur(stopTime, startTime)
+		infoString = hostname + " - " + data.Sessions[0].Creator_ip + "\nИгра: " + game
+		infoString += "\nПродолжительность сессии: " + sessionDur + "\nКомментарий: " + comment
+		fmt.Println(infoString)
+		fmt.Println()
+		return
+	}
+}
+
+// конвертирование даты и времени
+func dateTimeS(data int64) (string, time.Time) {
+
+	// Создание объекта времени
+	seconds := int64(data / 1000)
+	nanoseconds := int64((data % 1000) * 1000000)
+	t := time.Unix(seconds, nanoseconds)
+
+	// Форматирование времени
+	formattedTime := t.Format(timeFormat)
+
+	return formattedTime, t
+}
+
+func dur(stopTime, startTime time.Time) (sessionDur string) {
+	duration := stopTime.Sub(startTime).Round(time.Second)
+	hours := int(duration.Hours())
+	minutes := int(duration.Minutes()) % 60
+	seconds := int(duration.Seconds()) % 60
+	hou := strconv.Itoa(hours)
+	sessionDur = ""
+	if hours < 2 {
+		sessionDur = sessionDur + "0" + hou + ":"
+	} else {
+		sessionDur = sessionDur + hou + ":"
+	}
+	min := strconv.Itoa(minutes)
+	if minutes < 10 {
+		sessionDur = sessionDur + "0" + min + ":"
+	} else {
+		sessionDur = sessionDur + min + ":"
+	}
+	sec := strconv.Itoa(seconds)
+	if seconds < 10 {
+		sessionDur = sessionDur + "0" + sec
+	} else {
+		sessionDur = sessionDur + sec
+	}
+	return
+}
+
+func ipInf(ip string) (string, string, string) {
+	apiURL := fmt.Sprintf("https://ipinfo.io/%s/json", ip)
+
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	var ipInfo IPInfoResponse
+	err = json.NewDecoder(resp.Body).Decode(&ipInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ipInfo.City, ipInfo.ISP, ipInfo.Region
+}
