b6848b43f0b0bdea65da73d5b78c7e013bb316ba
IceBearG <65737922+IceBeerG@users.noreply.github.com>
2023-11-11 20:59:54 +0400

    Add files via upload

--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.21.2
 require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 
 require golang.org/x/sys v0.14.0
+
+require github.com/oschwald/maxminddb-golang v1.12.0
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
@@ -17,14 +18,15 @@ import (
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/oschwald/maxminddb-golang"
 	"golang.org/x/sys/windows/registry"
 )
 
 var (
-	BotToken, fileConfig, fileGames string
-	serverID, authToken, hostname   string
-	Chat_IDint                      int64
-	isRunning                       bool
+	BotToken, fileConfig, fileGames, hostname      string
+	serverID, authToken, mmdbASN, mmdbCity, ipInfo string
+	Chat_IDint                                     int64
+	isRunning, onlineIpInfo                        bool
 )
 
 const (
@@ -36,13 +38,6 @@ const (
 type Product struct {
 	ProductID string `json:"productId"`
 	Title     string `json:"title"`
-}
-
-type IPInfoResponse struct {
-	IP     string `json:"ip"`
-	City   string `json:"city"`
-	Region string `json:"region"`
-	ISP    string `json:"org"`
 }
 
 // структура для выгрузки информации по сессиям
@@ -59,10 +54,38 @@ type SessionsData struct {
 	}
 }
 
+// для получения провайдера
+type ASNRecord struct {
+	AutonomousSystemNumber       uint32 `maxminddb:"autonomous_system_number"`
+	AutonomousSystemOrganization string `maxminddb:"autonomous_system_organization"`
+}
+
+// для получения города
+type CityRecord struct {
+	City struct {
+		Names map[string]string `maxminddb:"names"`
+	} `maxminddb:"city"`
+	Country struct {
+		Names map[string]string `maxminddb:"names"`
+	} `maxminddb:"country"`
+	Location struct {
+		Latitude  float64 `maxminddb:"latitude"`
+		Longitude float64 `maxminddb:"longitude"`
+	} `maxminddb:"location"`
+}
+
+type IPInfoResponse struct {
+	IP     string `json:"ip"`
+	City   string `json:"city"`
+	Region string `json:"region"`
+	ISP    string `json:"org"`
+}
+
 func main() {
 	// Следующие 2 строки вводим свои данные. Чат ID будет с - в начале если это общий чат, и без - если это личка
-	// BotToken = "11111111:sdsdfsdde" 			// токен бота
-	// Chat_IDint = -1111111                	// определяем ID чата получателя
+	BotToken = "11111111:sdsdfsdde" // токен бота
+	Chat_IDint = -1111111           // определяем ID чата получателя
+	onlineIpInfo = true             // false - инфо по IP используя оффлайн базу GeoLite, true - инфо по IP через сайт ipinfo.io
 
 	logFilePath := "log.log" // Имя файла для логирования ошибок
 	logFilePath = filepath.Join(filepath.Dir(os.Args[0]), logFilePath)
@@ -84,20 +107,65 @@ func main() {
 
 	fileGames = filepath.Join(dir, "games.txt")
 	fileConfig = filepath.Join(dir, "config.txt")
+
+	//блок для получения данных из конфига
+	_, err = os.Stat(fileConfig)
+	if os.IsNotExist(err) {
+		// Файл не существует
+		log.Printf("Файл %s не существует. %s. %s\n", fileConfig, err, getLine())
+	} else {
+		bToken, err := readConfig("tokenbot", fileConfig)
+		if err != nil {
+			log.Printf("Ошибка - %s. %s\n", err, getLine())
+		}
+		if bToken != "" {
+			BotToken = bToken // получаем токен этого бота
+		}
+		Chat_ID, err := readConfig("chatID", fileConfig) // определяем ID чата
+		if err != nil {
+			log.Printf("Ошибка - %s. %s\n", err, getLine())
+		}
+		if Chat_ID != "" {
+			Chat_IDint, err = strconv.ParseInt(Chat_ID, 10, 64) // конвертируем ID чата в int64
+			if err != nil {
+				log.Println("Error: ", err, getLine())
+			}
+		}
+		onlineIpInf, err := readConfig("onlineIpInfo", fileConfig) // определяем ID чата
+		if err != nil {
+			log.Printf("Ошибка - %s. %s\n", err, getLine())
+		}
+		if onlineIpInf == "true" {
+			onlineIpInfo = true
+		} else if onlineIpInf == "false" {
+			onlineIpInfo = false
+		}
+	}
+
+	mmdbASN = filepath.Join(dir, "GeoLite2-ASN.mmdb")
+	mmdbCity = filepath.Join(dir, "GeoLite2-City.mmdb")
+	_, err = os.Stat(mmdbASN)
+	if os.IsNotExist(err) {
+		// Файл не существует
+		onlineIpInfo = true
+		log.Printf("Файл %s не существует. %s. %s\n", fileConfig, err, getLine())
+	} else {
+		_, err = os.Stat(mmdbCity)
+		if os.IsNotExist(err) {
+			// Файл не существует
+			onlineIpInfo = true
+			log.Printf("Файл %s не существует. %s. %s\n", fileConfig, err, getLine())
+		} else {
+			log.Println(mmdbASN)
+			log.Println(mmdbCity)
+			go updateRestart(mmdbASN, mmdbCity)
+		}
+	}
+
 	log.Println(fileGames)
 	log.Println(fileConfig)
 
 	gameID(fileGames) // получение списка ID игры - Название игры и сохранение в файл gamesID.txt
-
-	//блок для получения данных из конфига
-	// /*
-	BotToken, _ = readConfig("tokenbot", fileConfig)     // получаем токен этого бота
-	Chat_ID, _ := readConfig("chatID", fileConfig)       // определяем ID чата
-	Chat_IDint, err := strconv.ParseInt(Chat_ID, 10, 64) // конвертируем ID чата в int64
-	if err != nil {
-		log.Println("Error: ", err, getLine())
-	}
-	// */
 
 	regFolder := `SOFTWARE\ITKey\Esme`
 	serverID = regGet(regFolder, "last_server") // получаем ID сервера
@@ -289,22 +357,44 @@ func sessionInfo(status string) (infoString string) {
 	comment := strings.ReplaceAll(data.Sessions[0].Abort_comment, ";", ":")
 	sessionOn, _ := dateTimeS(data.Sessions[0].Created_on)
 	game, _ := readConfig(data.Sessions[0].Product_id, fileGames)
-	city, asn, region := ipInf(data.Sessions[0].Creator_ip)
+	game = "\nИгра: " + game
+	ipInfo = ""
+	if onlineIpInfo {
+		ipInfo = ipInf(data.Sessions[0].Creator_ip)
+	} else {
+		ip := net.ParseIP(data.Sessions[0].Creator_ip)
+		cityRecord, asnRecord, err := getASNRecord(mmdbCity, mmdbASN, ip)
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		asn := asnRecord.AutonomousSystemOrganization // провайдер клиента
+		city := cityRecord.City.Names["ru"]           // город клиента
+		if city != "" {
+			ipInfo = "\nГород: " + city
+		}
+		if asn != "" {
+			ipInfo += "\nПровайдер: " + asn
+		}
+
+	}
 	if status == "Start" { // формируем текст для отправки
-		infoString = hostname + " - " + data.Sessions[0].Creator_ip + "\n\n" + sessionOn + "\nИгра: " + game
-		infoString += "\nГород: " + city + "\nРегион: " + region + "\nПровайдер: " + asn + "\nОплата: " + data.Sessions[0].Billing_type
+
+		billing := data.Sessions[0].Billing_type
+		if billing != "" {
+			billing = "\nОплата: " + data.Sessions[0].Billing_type
+		}
+		infoString = "[Start]\n" + hostname + " - " + data.Sessions[0].Creator_ip + "\n\n" + sessionOn + game + ipInfo + billing
 		fmt.Println()
 		return
 	} else { // высчитываем продолжительность сессии и формируем текст для отправки
 		_, stopTime := dateTimeS(data.Sessions[0].Finished_on)
 		_, startTime := dateTimeS(data.Sessions[0].Created_on)
-		sessionDur := dur(stopTime, startTime)
-		log.Printf("Начало сессии: %d -> %s.\n", data.Sessions[0].Created_on, startTime)
-		log.Printf("Завершение сессии: %d -> %s.\n", data.Sessions[0].Finished_on, stopTime)
-		log.Printf("Продолжительность сессии: %s", sessionDur)
-		infoString = hostname + " - " + data.Sessions[0].Creator_ip + "\nИгра: " + game
-		infoString += "\nПродолжительность сессии: " + sessionDur + "\nКомментарий: " + comment
+		sessionDur := "\nПродолжительность сессии: " + dur(stopTime, startTime)
+		if comment != "" {
+			comment = "\nКомментарий: " + comment
+		}
+		infoString = "[Finish]\n" + hostname + " - " + data.Sessions[0].Creator_ip + game + sessionDur + comment
 		fmt.Println()
 		return
 	}
@@ -326,15 +416,10 @@ func dateTimeS(data int64) (string, time.Time) {
 
 func dur(stopTime, startTime time.Time) (sessionDur string) {
 	duration := stopTime.Sub(startTime).Round(time.Second)
-	log.Println("func dur.duration - ", duration)
 	hours := int(duration.Hours())
-	log.Println("func dur.hours - ", hours)
 	minutes := int(duration.Minutes()) % 60
-	log.Println("func dur.minutes - ", minutes)
 	seconds := int(duration.Seconds()) % 60
-	log.Println("func dur.seconds - ", seconds)
 	hou := strconv.Itoa(hours)
-	log.Println("func dur.hou - ", hou)
 	sessionDur = ""
 	if hours < 2 {
 		sessionDur = sessionDur + "0" + hou + ":"
@@ -342,38 +427,18 @@ func dur(stopTime, startTime time.Time) (sessionDur string) {
 		sessionDur = sessionDur + hou + ":"
 	}
 	min := strconv.Itoa(minutes)
-	log.Println("func dur.min - ", min)
 	if minutes < 10 {
 		sessionDur = sessionDur + "0" + min + ":"
 	} else {
 		sessionDur = sessionDur + min + ":"
 	}
 	sec := strconv.Itoa(seconds)
-	log.Println("func dur.sec - ", sec)
 	if seconds < 10 {
 		sessionDur = sessionDur + "0" + sec
 	} else {
 		sessionDur = sessionDur + sec
 	}
 	return
-}
-
-func ipInf(ip string) (string, string, string) {
-	apiURL := fmt.Sprintf("https://ipinfo.io/%s/json", ip)
-
-	resp, err := http.Get(apiURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	var ipInfo IPInfoResponse
-	err = json.NewDecoder(resp.Body).Decode(&ipInfo)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return ipInfo.City, ipInfo.ISP, ipInfo.Region
 }
 
 // получаем данные из реестра
@@ -392,4 +457,109 @@ func regGet(regFolder, keys string) string {
 	// log.Printf("%s - %s: %s\n", keys, regFolder, value)
 
 	return value
+}
+
+// offline инфо по IP
+func getASNRecord(mmdbCity, mmdbASN string, ip net.IP) (*CityRecord, *ASNRecord, error) {
+	dbASN, err := maxminddb.Open(mmdbASN)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer dbASN.Close()
+
+	var recordASN ASNRecord
+	err = dbASN.Lookup(ip, &recordASN)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	db, err := maxminddb.Open(mmdbCity)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer db.Close()
+
+	var recordCity CityRecord
+	err = db.Lookup(ip, &recordCity)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &recordCity, &recordASN, err
+}
+
+func ipInf(ip string) string {
+	apiURL := fmt.Sprintf("https://ipinfo.io/%s/json", ip)
+
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	var ipInfo IPInfoResponse
+	err = json.NewDecoder(resp.Body).Decode(&ipInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text := "\nГород: " + ipInfo.City + "\nРегион: " + ipInfo.Region + "\nПровайдер: " + ipInfo.ISP
+	return text
+}
+
+func updateRestart(mmdbASN, mmdbCity string) {
+	var previousModTime1, previousModTime2 time.Time
+	filePath1 := mmdbASN
+	filePath2 := mmdbCity
+	// Получаем информацию о файлах
+	fileInfo1, err := os.Stat(filePath1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileInfo2, err := os.Stat(filePath2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Проверяем время последнего изменения файла
+	previousModTime1 = fileInfo1.ModTime()
+	previousModTime2 = fileInfo2.ModTime()
+	for {
+		time.Sleep(60 * time.Second)        // Интервал повторной проверки
+		fileInfo1, err = os.Stat(filePath1) // Повторно получаем информацию о файле
+		if err != nil {
+			log.Fatal(err)
+		}
+		fileInfo2, err = os.Stat(filePath2)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Проверяем, изменился ли файл по сравнению с предыдущим временем модификации
+		if previousModTime1 != fileInfo1.ModTime() || previousModTime2 != fileInfo2.ModTime() {
+			log.Println("Файл был изменен. Перезапуск приложения...")
+			restart()
+		}
+	}
+}
+
+func restart() {
+	// Получаем путь к текущему исполняемому файлу
+	execPath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Запускаем новый экземпляр приложения с помощью os/exec
+	cmd := exec.Command(execPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Запускаем новый процесс и не ждем его завершения
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Завершаем текущий процесс
+	os.Exit(0)
 }
